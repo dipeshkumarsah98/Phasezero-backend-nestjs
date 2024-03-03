c200bded950c34810f1968e6c5ceba4c0e11dfa1
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2024-03-03 13:00:58 +0545

    feat: project setup

---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+import * as _ from 'lodash';
+
+export const users = [
+  {
+    name: 'Dipesh kumar sah',
+    email: 'dipesh@gmail.com',
+    phone: '1234567890',
+    address: 'Kathmandu, Nepal',
+  },
+  {
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    phone: '1234567890',
+    address: 'Kathmandu, Nepal',
+  },
+];
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for await (const user of users) {
+    const userAttrs = _.cloneDeep(user);
+    await prisma.user.create({
+      data: {
+        ...userAttrs,
+      },
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async error => {
+    console.log(error);
+    await prisma.$disconnect();
+  });
